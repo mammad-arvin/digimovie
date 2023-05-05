8b58fb00a4ee57e935291863b9c57ad1ff783cb8
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-05-05 21:27:15 +0430

    genre and title changer  function added

--- src/helpers/helperFunctions.js
+++ src/helpers/helperFunctions.js
@@ -1,9 +1,26 @@
-const miniName=(name)=>{
-    if(name.length >= 6){
-        const newName=name.split("").splice(0,5);
-        return newName.join("") +"..."
-    }else{
-        return name
+// minimize name of profile of username
+const miniName = (name) => {
+    if (name.length >= 6) {
+        const newName = name.split("").splice(0, 5);
+        return newName.join("") + "...";
+    } else {
+        return name;
     }
-}
-export { miniName}
+};
+
+//  join genres for show together
+const joinGenre = (manyRefrence) => {
+    const Genres = manyRefrence.filter((refrence) =>
+        refrence.__typename==="Genres" && refrence.genre
+    );
+    const Genre = Genres.map((gen) => gen.genre )
+    return Genre.join(" , ");
+};
+
+
+// change title of any page to special title that page
+const titleChanger = (title) => {
+    document.title = title;
+};
+
+export { miniName, titleChanger, joinGenre };
