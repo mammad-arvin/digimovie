4c4cf129b51122031223daf3cbed49c8e4acdb50
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-05-24 19:48:35 +0330

    layout completed

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,33 +1,38 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import { Container, Stack} from "@mui/material";
-
-// mui
-import { useTheme } from "@emotion/react";
 
 // component
+import Header from "./Header";
+import Footer from "./Footer";
 
+// mui
+import { Box, Container, Stack } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const Layout = ({ children }) => {
     const mode = useTheme();
 
     return (
-        <>
-                <Container
-                    maxWidth="xl"
-                    sx={{
-                        overflowX: "hidden",
-                        background: `${
-                            mode.palette.mode === "dark" ? "#050505" : "#F2F2F2"
-                        }`,
-                    }}
-                >
-                    <Header/>
-                    <Stack sx={{ minHeight: "700px" }}>{children}</Stack>
-                    <Footer />
-                </Container>
-        </>
+        <Box
+            sx={{
+                minHeight:"100vh",
+                background:
+                    mode.palette.mode === "dark" ? "#050505" : "#F2F2F2",
+            }}
+        >
+            <Container
+                maxWidth="xl"
+                sx={{
+                    overflowX: "hidden",
+                    background: `${
+                        mode.palette.mode === "dark" ? "#050505" : "#F2F2F2"
+                    }`,
+                }}
+            >
+                <Header />
+                <Stack sx={{ minHeight: "700px" }}>{children}</Stack>
+                <Footer />
+            </Container>
+        </Box>
     );
 };
 
