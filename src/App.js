3f87e2d0d3ae26f6fee89008afc4612f8cf33403
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-03 18:47:30 +0330

    rout of news added with slug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+// roter dom
+import { Routes, Route } from "react-router-dom";
 
 // components
 import Layout from "./Layout/index";
+import Report from "./components/news/Report";
 
 function App() {
     return (
         <Layout>
-            
+            <Routes>
+                <Route path="/news/:LocalSlug?" element={<Report />} />
+            </Routes>
         </Layout>
     );
 }
