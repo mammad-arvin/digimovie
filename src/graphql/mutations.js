e6465e7704e3ced94cd9e22c68732b0b82d85875
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-07 11:45:29 +0330

    add a mutation for increase or decrease liks by registred user

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -12,4 +12,16 @@ const SEND_PAGE_VIEWS = gql`
     }
 `;
 
-export { SEND_PAGE_VIEWS };
+// in_de_crease liks 
+const iNCREASE_AND_DECREASE_LIKS = gql`
+    mutation changerLikes($slug: String! , $liks: Int!){
+        updateNews(data: { liks: $liks }, where: { slug: $slug }){
+            id
+        }
+        publishNews(where: { slug: $slug }) {
+            id
+        }
+    }
+`;
+
+export { SEND_PAGE_VIEWS, iNCREASE_AND_DECREASE_LIKS };
