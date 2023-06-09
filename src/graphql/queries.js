d0f6c4b5581afa5b819671cb2fac684d0f01812d
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-10 02:10:57 +0330

    query for get user Id by email or userName

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -68,6 +68,25 @@ const GET_REPORT_OF_NEWS = gql`
     }
 `;
 
+// get ueser id when user signing in
+const GET_DATA_OF_USER_FOR_LOGE_IN_WITH_USERNAME = gql`
+    query MyQuery($name_email: String!) {
+        registeredUser(where: { userName: $name_email }) {
+            id
+            psassword
+        }
+    }
+`;
+
+const GET_DATA_OF_USER_FOR_LOGE_IN_WITH_EMAIL = gql`
+    query MyQuery($name_email: String!) {
+        registeredUser(where: { email: $name_email }) {
+            id
+            psassword
+        }
+    }
+`;
+
 // get username of loged in user
 const GET_USER_INFO_FOR_LOGED_IN_USER = gql`
     query MyQuery($userId: ID!) {
@@ -82,5 +101,7 @@ export {
     GET_FILMS_DETAILS_FOR_SEARCH,
     GET_FOOTER_NEWS_GALLERY,
     GET_REPORT_OF_NEWS,
+    GET_DATA_OF_USER_FOR_LOGE_IN_WITH_USERNAME,
+    GET_DATA_OF_USER_FOR_LOGE_IN_WITH_EMAIL,
     GET_USER_INFO_FOR_LOGED_IN_USER,
 };
