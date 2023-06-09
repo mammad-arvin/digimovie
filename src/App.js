f0ddaf72e481ba0015e0c5c7ff771b789584426c
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-10 02:13:45 +0330

    route of Sign in added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Routes, Route } from "react-router-dom";
 // components
 import Layout from "./Layout/index";
 import Report from "./components/news/Report";
+import SignIn from "./components/signIn_signUp/SignIn";
 
 function App() {
     return (
         <Layout>
             <Routes>
                 <Route path="/news/:LocalSlug?" element={<Report />} />
+                <Route path="/signin" element={<SignIn/>} />
             </Routes>
         </Layout>
     );
