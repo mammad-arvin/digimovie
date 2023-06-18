b8ebb03d765869150bf7bdb2b1fb957db231b6f0
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-18 13:32:32 +0330

    signUp route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout/index";
 import Report from "./components/news/Report";
 import SignIn from "./components/signIn_signUp/SignIn";
+import SignUp from "./components/signIn_signUp/SignUp";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                 <Route path="/news/:LocalSlug?" element={<Report />} />
                 <Route path="/signin" element={<SignIn/>} />
+                <Route path="/signup" element={<SignUp/>} />
             </Routes>
         </Layout>
     );
