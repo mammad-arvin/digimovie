c6e01c65118bfeae8d74c47d5fc0e23027c90588
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-06-23 18:20:09 +0330

    column reverse to show newest comment in top :)

--- src/shared/Comments/showComment/ShowComments.js
+++ src/shared/Comments/showComment/ShowComments.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+// MUI
+import { Box } from "@mui/material";
+
 // component
 import EveryComment from "./EveryComment";
 
 const ShowComments = ({ comments }) => {
     return (
-        <>
+        <Box display={"flex"} flexDirection={"column-reverse"}>
             {comments.map((item) => {
                 return <EveryComment key={item.id} item={item} />;
             })}
-        </>
+        </Box>
     );
 };
 
