2a2cba441a145685857f9ae9c5f48ce7efb91f17
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-07-01 11:36:23 +0330

    path added and some style of movie details modified to center

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // MUI
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { useTheme } from "@emotion/react";
 
 // mutation
@@ -10,6 +10,9 @@ import { GET_MOVIE_DATA } from "../../graphql/queries";
 
 // router dom
 import { useParams } from "react-router-dom";
+
+// components
+import Path from "../../shared/Path";
 
 const Movie = () => {
     const {
@@ -25,17 +28,31 @@ const Movie = () => {
     });
     data && console.log(data);
 
+    if (data) {
+        var { title, slug } = data.movie;
+    }
     return (
         <>
+            {/* path */}
+            <Path
+                category={{ t: "دانلود فیلم", l: "/movies" }}
+                title={"دانلود فیلم" + " " + title}
+                slug={slug}
+            />
+
+            {/* movie details */}
             {data && (
                 <Box
                     color={text.primary}
+                    display={"flex"}
+                    justifyContent={"center"}
                     mt="30px"
                     zIndex={1}
-                    width={"110%"}
+                    width={"105%"}
                     height={"800px"}
                     sx={{
                         background: `url(${data.movie.images[0].url})`,
+                        backgroundPosition: "center",
                         backgroundSize: "cover",
                     }}
                 >
@@ -45,7 +62,21 @@ const Movie = () => {
                             height: "100%",
                             backdropFilter: "brightness(50%)",
                         }}
-                    ></Box>
+                    >
+                        <Grid
+                            container
+                            display={"flex"}
+                            justifyContent={"center"}
+                        >
+                            <Grid
+                                item
+                                xs={12}
+                                md={10.25}
+                                display={"flex"}
+                                justifyContent={"center"}
+                            ></Grid>
+                        </Grid>
+                    </Box>
                 </Box>
             )}
         </>
