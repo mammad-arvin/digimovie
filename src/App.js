55c8da6151eff00228a118b363a37a3e3858eeb3
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-07-03 02:13:27 +0330

    Route of movie added and news route modified.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import Report from "./components/news/Report";
 import SignIn from "./components/signIn_signUp/SignIn";
 import SignUp from "./components/signIn_signUp/SignUp";
 import Home from "./components/Home/Home";
+import Movie from "./components/Movie/Movie";
 
 function App() {
     return (
         <Layout>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/news/:LocalSlug?" element={<Report />} />
+                <Route path="/news/:LocalSlug" element={<Report />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/movies/:LocalSlug" element={<Movie />} />
             </Routes>
         </Layout>
     );
