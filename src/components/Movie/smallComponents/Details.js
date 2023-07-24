46a2a8e70a46a3ab6a5167719d842ed2c25813a8
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-07-24 11:55:51 +0330

    little decription added

--- src/components/Movie/smallComponents/Details.js
+++ src/components/Movie/smallComponents/Details.js
@@ -5,9 +5,11 @@ import HederOfDetils from "./HederOfDetils";
 import InfoOfDetails from "./InfoOfDetails";
 
 // MUI
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 const Details = ({ data }) => {
+    const { littleDescription } = data;
+
     return (
         <>
             {/* header details component */}
@@ -16,6 +18,15 @@ const Details = ({ data }) => {
             {/* details */}
             <Grid container>
                 <InfoOfDetails data={data} />
+            </Grid>
+
+            {/* little description */}
+            <Grid container>
+                <Grid item xs={12} p={"0 10px"}>
+                    <Typography variant="p" fontSize="12.8px" lineHeight="22px">
+                        {littleDescription}
+                    </Typography>
+                </Grid>
             </Grid>
         </>
     );
