03ac3f923982c2be4fce8192436c30973465492d
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-08-26 20:48:06 +0330

    route of movies component to show movie poster added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Report from "./components/news/Report";
 import SignIn from "./components/signIn_signUp/SignIn";
 import SignUp from "./components/signIn_signUp/SignUp";
 import Home from "./components/Home/Home";
+import Movies from "./components/Movies/Movies";
 import Movie from "./components/Movie/Movie";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/news/:LocalSlug" element={<Report />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/movies" element={<Movies />} />
                 <Route path="/movies/:LocalSlug" element={<Movie />} />
             </Routes>
         </Layout>
