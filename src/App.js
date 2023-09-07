6a517f0931b1b96c0975e34252ccae558d03b7ba
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-09-07 23:29:14 +0330

    route of favorite movie component added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignUp from "./components/signIn_signUp/SignUp";
 import Home from "./components/Home/Home";
 import Movies from "./components/Movies/Movies";
 import Movie from "./components/Movie/Movie";
+import FavoriteMovies from "./components/Favorite movie/FavoriteMovies";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/movies" element={<Movies />} />
                 <Route path="/movies/:LocalSlug" element={<Movie />} />
+                <Route path="/favorite" element={<FavoriteMovies />} />
             </Routes>
         </Layout>
     );
