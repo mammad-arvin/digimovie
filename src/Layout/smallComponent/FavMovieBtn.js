16b084e1459bd4a0d83b6af2f1c62681dda74c06
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-09-07 11:36:37 +0330

    badge content counter now is favorited movies length , and if user are logged in so favMovieBTN is show

--- src/Layout/smallComponent/FavMovieBtn.js
+++ src/Layout/smallComponent/FavMovieBtn.js
@@ -11,11 +11,11 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const innerWidth=window.innerWidth;
+const innerWidth = window.innerWidth;
 
 const FavMovie = styled.div`
-    height: ${innerWidth < 460 ? "47px" : "57px" } ;
-    width: ${innerWidth < 460 ? "47px" : "57px" };
+    height: ${innerWidth < 460 ? "47px" : "57px"};
+    width: ${innerWidth < 460 ? "47px" : "57px"};
     display: flex;
     justify-content: center;
     align-items: center;
@@ -23,8 +23,8 @@ const FavMovie = styled.div`
     margin-left: 16px;
     background: ${(props) => (props.mode === "dark" ? "#131313" : "#CCCCCC")};
     .innerCicle {
-        height: ${innerWidth < 460 ? "32px" : "38px" };
-        width: ${innerWidth < 460 ? "32px" : "38px" };
+        height: ${innerWidth < 460 ? "32px" : "38px"};
+        width: ${innerWidth < 460 ? "32px" : "38px"};
         display: flex;
         justify-content: center;
         align-items: center;
@@ -32,8 +32,8 @@ const FavMovie = styled.div`
         background-color: ${(props) =>
             props.mode === "dark" ? "#252525" : "#FFF"};
         .favIcon {
-            height: ${innerWidth < 460 ? "10px" : "20px" };
-            width: ${innerWidth < 460 ? "10px" : "20px" };
+            height: ${innerWidth < 460 ? "10px" : "20px"};
+            width: ${innerWidth < 460 ? "10px" : "20px"};
             color: #eb8307;
             display: flex;
             justify-content: center;
@@ -47,26 +47,35 @@ const FavMovieBtn = () => {
     const {
         palette: { mode },
     } = useTheme();
+
+    // get from local
+    const favedMovie = JSON.parse(localStorage.getItem("favoriteMovies"));
+    const userId = localStorage.getItem("userId");
+
     return (
-        <Link to="/favorite">
-            <FavMovie mode={mode}>
-                <Box className="innerCicle">
-                    <Box className="favIcon">
-                        <Badge
-                            badgeContent={9}
-                            color="favMovie"
-                            showZero
-                            sx={{
-                                position: "absolute",
-                                top: "8px",
-                                right: "5px",
-                            }}
-                        />
-                        <FavoriteBorderIcon sx={{fontSize:{xs:"large" , sm:"25px"}}} />
+        userId && (
+            <Link to="/favorite">
+                <FavMovie mode={mode}>
+                    <Box className="innerCicle">
+                        <Box className="favIcon">
+                            <Badge
+                                badgeContent={favedMovie.length}
+                                color="favMovie"
+                                showZero
+                                sx={{
+                                    position: "absolute",
+                                    top: "8px",
+                                    right: "5px",
+                                }}
+                            />
+                            <FavoriteBorderIcon
+                                sx={{ fontSize: { xs: "large", sm: "25px" } }}
+                            />
+                        </Box>
                     </Box>
-                </Box>
-            </FavMovie>
-        </Link>
+                </FavMovie>
+            </Link>
+        )
     );
 };
 
