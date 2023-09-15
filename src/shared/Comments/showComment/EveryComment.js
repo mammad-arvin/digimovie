d57c8c7dd447b1c3da1d4ffb57132fb42851bef9
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2023-09-15 11:35:48 +0330

    like and dislike of comment handled , alert done for it

--- src/shared/Comments/showComment/EveryComment.js
+++ src/shared/Comments/showComment/EveryComment.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 // MUI
 import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 // icons
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
@@ -13,6 +15,10 @@ import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
 
 // component
 import ShowTimeAgo from "../../ShowTimeAgo";
+
+// mutation
+import { useMutation } from "@apollo/client";
+import { UPDATE_COMMENT_AND_USER_AFTER_LIKE_DISLIKE } from "../../../graphql/mutations";
 
 // style
 import styled from "styled-components";
@@ -28,6 +34,11 @@ const StackStyled = styled(Stack)`
     cursor: pointer;
 `;
 
+// for alert
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const EveryComment = ({ item }) => {
     const {
         palette: { mode, text },
@@ -35,6 +46,140 @@ const EveryComment = ({ item }) => {
 
     // show spile comment
     const [showSpoil, setShowSpoil] = useState(false);
+
+    // mutation
+    const [updateData] = useMutation(
+        UPDATE_COMMENT_AND_USER_AFTER_LIKE_DISLIKE
+    );
+
+    // for alert
+    const [up_succ, setUp_succ] = useState(false);
+    const [up_war, setUp_war] = useState(false);
+    const [dis_succ, setDis_succ] = useState(false);
+    const [dis_war, setDis_war] = useState(false);
+
+    // like and dislike handler
+    // initial value of data
+    const [uplikes, setUplikes] = useState(Number(item.upLikes));
+    const [dislikes, setDislikes] = useState(Number(item.disLikes));
+    const dislikedComments = JSON.parse(
+        localStorage.getItem("dislikedComments")
+    );
+    const likedCommets = JSON.parse(localStorage.getItem("likedCommets"));
+
+    // was liked or disliked?
+    const [wasLiked, setWasLiked] = useState(false);
+    !wasLiked &&
+        likedCommets.map((itm) => itm === item.id && setWasLiked(true));
+    const [wasDisliked, setWasDisliked] = useState(false);
+    !wasDisliked &&
+        dislikedComments.map((itm) => itm === item.id && setWasDisliked(true));
+
+    // like handler
+    const likeHandler = () => {
+        if (!wasLiked) {
+            // remove from dislikes
+            dislikedComments.map(
+                (i, index) => i === item.id && dislikedComments.splice(index, 1)
+            );
+            wasDisliked && setDislikes(dislikes - 1);
+
+            // push to uplikes && increase uplikes
+            likedCommets.push(item.id);
+            setUplikes(uplikes + 1);
+            setWasLiked(true);
+
+            // submit to localstorage and graph backend
+            wasDisliked
+                ? submitToLocal_Graph(
+                      uplikes + 1,
+                      dislikes - 1,
+                      likedCommets,
+                      dislikedComments
+                  )
+                : submitToLocal_Graph(
+                      uplikes + 1,
+                      dislikes,
+                      likedCommets,
+                      dislikedComments
+                  );
+
+            //   for dislike agin
+            setWasDisliked(false);
+
+            // show alert
+            setUp_succ(true);
+        } else {
+            setUp_war(true);
+        }
+    };
+
+    // dislike handler
+
+    const dislikeHandler = () => {
+        if (!wasDisliked) {
+            // remove from liked
+            likedCommets.map(
+                (i, index) => i === item.id && likedCommets.splice(index, 1)
+            );
+            wasLiked && setUplikes(uplikes - 1);
+
+            // push to dislikes && increase dislikes
+            dislikedComments.push(item.id);
+            setDislikes(dislikes + 1);
+            setWasDisliked(true);
+
+            // submit to localstorage and graph backend
+            wasLiked
+                ? submitToLocal_Graph(
+                      uplikes - 1,
+                      dislikes + 1,
+                      likedCommets,
+                      dislikedComments
+                  )
+                : submitToLocal_Graph(
+                      uplikes,
+                      dislikes + 1,
+                      likedCommets,
+                      dislikedComments
+                  );
+
+            // for like agin
+            setWasLiked(false);
+
+            // show alert
+            setDis_succ(true);
+        } else {
+            setDis_war(true);
+        }
+    };
+
+    // submit to local and grah handler
+    const submitToLocal_Graph = (
+        uplikes,
+        dislikes,
+        likedCommets,
+        dislikedComments
+    ) => {
+        // TO LOCAL
+        localStorage.setItem("likedCommets", JSON.stringify(likedCommets));
+        localStorage.setItem(
+            "dislikedComments",
+            JSON.stringify(dislikedComments)
+        );
+
+        // TO GRAPH
+        updateData({
+            variables: {
+                id: item.id,
+                uplikes,
+                dislikes,
+                userName: item.registeredUsercomment.userName,
+                likedCommets,
+                dislikedComments,
+            },
+        });
+    };
 
     return (
         <Box
@@ -70,26 +215,26 @@ const EveryComment = ({ item }) => {
                             {/* dislikes */}
                             <StackStyled
                                 mode={mode}
-                                onClick={() => alert("توسعه داده نشده")}
+                                onClick={() => dislikeHandler()}
                             >
                                 <ThumbDownOffAltOutlinedIcon
                                     sx={{ color: "#E91E63" }}
                                 />
                                 <Typography variant="subtitle2">
-                                    {Number(item.disLikes)}
+                                    {dislikes}
                                 </Typography>
                             </StackStyled>
 
                             {/* uplikes */}
                             <StackStyled
                                 mode={mode}
-                                onClick={() => alert("توسعه داده نشده")}
+                                onClick={() => likeHandler()}
                             >
                                 <ThumbUpOutlinedIcon
                                     sx={{ color: "#8BC34A" }}
                                 />
                                 <Typography variant="subtitle2">
-                                    {Number(item.upLikes)}
+                                    {uplikes}
                                 </Typography>
                             </StackStyled>
                         </Box>
@@ -239,6 +384,41 @@ const EveryComment = ({ item }) => {
                     </Button>
                 </Box>
             )}
+
+            {/* show alert */}
+            {/* for up like */}
+            <Snackbar
+                open={up_succ}
+                autoHideDuration={1500}
+                onClose={() => setUp_succ(false)}
+            >
+                <Alert severity="success">لایک شما ثبت شد!</Alert>
+            </Snackbar>
+
+            <Snackbar
+                open={up_war}
+                autoHideDuration={1500}
+                onClose={() => setUp_war(false)}
+            >
+                <Alert severity="warning">قبلا لایک کرده اید</Alert>
+            </Snackbar>
+
+            {/* for dislike */}
+            <Snackbar
+                open={dis_succ}
+                autoHideDuration={1500}
+                onClose={() => setDis_succ(false)}
+            >
+                <Alert severity="success">لایک منفی شما ثبت شد!</Alert>
+            </Snackbar>
+
+            <Snackbar
+                open={dis_war}
+                autoHideDuration={1500}
+                onClose={() => setDis_war(false)}
+            >
+                <Alert severity="warning">قبلا لایک منفی کرده اید</Alert>
+            </Snackbar>
         </Box>
     );
 };
