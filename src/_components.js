77183ee389053dfe30f43829dee20b73484780e1
Aqil Elhaee Sahar <aqilhex@gmail.com>
2023-10-04 17:37:31 +0400

    All in one
    Let's import all components together for better components & screens managing

---
+++ src/_components.js
@@ -0,0 +1,20 @@
+import Layout from "./Layout/index";
+import Report from "./components/news/Report";
+import SignIn from "./components/signIn_signUp/SignIn";
+import SignUp from "./components/signIn_signUp/SignUp";
+import Home from "./components/Home/Home";
+import Movies from "./components/Movies/Movies";
+import Movie from "./components/Movie/Movie";
+import FavoriteMovies from "./components/Favorite movie/FavoriteMovies";
+import NotFound from "./components/NotFound";
+
+export default{
+    Layout,
+    Report,
+    SignIn,
+    SignUp,
+    Home,
+    Movies,
+    FavoriteMovies,
+    NotFound
+}
