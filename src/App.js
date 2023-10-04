9ff05c6cee6dda3534e1a452df77ce3ff6b23986
Aqil Elhaee Sahar <aqilhex@gmail.com>
2023-10-04 17:42:57 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 // components
-import Layout from "./Layout/index";
-import Report from "./components/news/Report";
-import SignIn from "./components/signIn_signUp/SignIn";
-import SignUp from "./components/signIn_signUp/SignUp";
-import Home from "./components/Home/Home";
-import Movies from "./components/Movies/Movies";
-import Movie from "./components/Movie/Movie";
-import FavoriteMovies from "./components/Favorite movie/FavoriteMovies";
-import NotFound from "./components/NotFound";
 import { Layout, Report, SignIn, SignUp, Home, Movies, Movie, FavoriteMovies, NotFound} from './_components';
 
 function App() {
